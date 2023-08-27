2a65f73503e56cb0bf946ab607af133357cd8792
Elliot Forst <epope@uw.edu>
2023-08-26 18:21:52 -0700

    feat: added ability to logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -15,28 +15,12 @@ class LoginController extends Controller
 {
     use AuthorizesRequests, ValidatesRequests;
 
-    // public function login(Request $request)
-	// {
-    //     $request->validate([
-    //         'name' => 'required',
-    //         'password' => '',
-    //     ]);
-
-    //     $user = User::where('name', $request->name)->first();
-
-    //     // TODO: check if a password is required for this user and validate
-    //     if (! $user || ! Hash::check($request->password, $user->password)) {
-    //         throw ValidationException::withMessages([
-    //             'name' => ['The provided credentials are incorrect.'],
-    //         ]);
-    //     }
-
-    //     return $user->createToken($request->device_name)->plainTextToken;
-	// }
-
     public function login(Request $request)
 	{
-
+        // TODO: When the mobile application uses the token to make an API request to your application, it should pass the token in the Authorization header as a Bearer token.
+        // TODO: get device name somehow for token name
+        // dd($request);
+        $device_name = 'test';
         $user = User::where('name', '=', $request->name)->first();
 
         // // TODO: check if a password is required for this user and validate
@@ -46,8 +30,21 @@ class LoginController extends Controller
         //     ]);
         // }
 
-        return $user->createToken('test')->plainTextToken;
+        return $user->createToken($device_name)->plainTextToken;
 	}
-}
 
-// TODO: When the mobile application uses the token to make an API request to your application, it should pass the token in the Authorization header as a Bearer token.
+    public function logout(Request $request)
+    {
+        if ($user = $request->user()) {
+            $user->currentAccessToken()->delete();
+
+            return response()->json([
+                'message' => 'User was successfully logged out.'
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'There were no logged in users.'
+        ], 200);
+    }
+}
